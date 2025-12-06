810cf81a5c4dab721b9373aa9702f76a8c5046d1
novgorodcev.k <novgorodcev.k@rwb.ru>
2025-12-06 23:31:11 +0300

    sdf

--- app/app.view.ts
+++ app/app.view.ts
@@ -1,13 +1,16 @@
 namespace $.$$ {
+	new $mol_after_frame(() => {
+		$hyoo_crus_yard.masters = ['https://crus.hd4.ru/'] // только прод-мастер
+		$hyoo_crus_glob.yard().sync() // дёрнуть синхронизацию
+	})
 
 	export class $bog_testops_app extends $.$bog_testops_app {
-		
-		@ $mol_mem
+		@$mol_mem
 		realm() {
 			return this.Realm()
 		}
 
-		@ $mol_mem
+		@$mol_mem
 		profile() {
 			return this.realm().home().hall_by($bog_testops_profile, {})
 		}
